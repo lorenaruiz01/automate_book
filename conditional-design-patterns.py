0999103d267ad7639a795995ba35dd2f620b622f
Lorena Ruiz <101694059+lorenaruiz01@users.noreply.github.com>
2026-05-23 14:50:11 -0700

    conditional

--- conditional-design-patterns.py
+++ conditional-design-patterns.py
@@ -12,3 +12,8 @@ else:
 
 # validating inputs
 filename = input("What would you like to name the photo?")
+
+if len(filename) < 3 or len(filename) > 20: 
+    # clear out the filename if it's invalid.
+    filename = ""
+    print("Filename must be between 3 and 20 characters.")
