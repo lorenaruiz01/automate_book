73e527b4bb2876ce34956182a5a5f69f91461187
Lorena Ruiz <101694059+lorenaruiz01@users.noreply.github.com>
2026-05-23 15:10:56 -0700

    conditional

--- conditional-design-patterns.py
+++ conditional-design-patterns.py
@@ -33,3 +33,9 @@ speed = 5
 acceleration = 5
 
 # Players unlock mega vehicles by winning a race on every course.
+if vehicle == "mega cycle":
+    acceleration = acceleration + 4
+elif vehicle == "mega kart":
+    speed = speed + 2
+    acceleration = acceleration + 2
+
