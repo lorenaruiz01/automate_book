40c6a890811b0cbd87835da9e871ad7ebb82be7b
Lorena Ruiz <101694059+lorenaruiz01@users.noreply.github.com>
2026-05-23 14:51:53 -0700

    conditional

--- conditional-design-patterns.py
+++ conditional-design-patterns.py
@@ -17,3 +17,7 @@ if len(filename) < 3 or len(filename) > 20:
     # clear out the filename if it's invalid.
     filename = ""
     print("Filename must be between 3 and 20 characters.")
+else: 
+    # photos are stored as JPEGs, so we add the file extension.
+    filename = filename + ".jpeg"
+    print("The file has been renamed to " + filename)
