02404e0f5b1336da46749ec6c0ee611d96f670c2
Lorena Ruiz <101694059+lorenaruiz01@users.noreply.github.com>
2026-09-10 13:55:30 -0700

    calculating quarters

--- A02-CS002.cpp
+++ A02-CS002.cpp
@@ -9,7 +9,7 @@ int main()
     saved_cents = total_cents; //preserving the original value so we can print at the end.
 
     quarters = total_cents / 25; //figures out how many quarters
-    total_cents - total_cents - (quarters * 25); //figures out how mny cents remain.
+    total_cents = total_cents - (quarters * 25); //figures out how mny cents remain.
 
     
 }
