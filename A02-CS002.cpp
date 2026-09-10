a0d540ae05b5aa52b447b741fb5cdc646d3e8f93
Lorena Ruiz <101694059+lorenaruiz01@users.noreply.github.com>
2026-09-10 13:50:50 -0700

    asking the user for input

--- A02-CS002.cpp
+++ A02-CS002.cpp
@@ -4,5 +4,9 @@ int main()
 {
     int saved_cents, total_cents, quarters, dimes, nickels, pennies;
 
+    std::cout << "Please enter a number greater than 500: ";
+    std:: cin >> total_cents;
+    saved_cents = total_cents; //preserving the original value so we can print at the end.
+
     
 }
