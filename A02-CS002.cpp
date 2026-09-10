b4aa62c5a50bf36c15f88b60a034028e365603a3
Lorena Ruiz <101694059+lorenaruiz01@users.noreply.github.com>
2026-09-10 13:52:21 -0700

    figuring out qtrs

--- A02-CS002.cpp
+++ A02-CS002.cpp
@@ -8,5 +8,8 @@ int main()
     std:: cin >> total_cents;
     saved_cents = total_cents; //preserving the original value so we can print at the end.
 
+    quarters = total_cents / 25; //figures out how many quarters
+    total_cents - total_cents - (quarters * 25); //figures out how mny cents remain.
+
     
 }
